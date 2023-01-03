622fd3e61b10a4523528ea7b52dbd0fd080584a4
Amirul Dev <73012169+amiruldev20@users.noreply.github.com>
2023-01-03 16:07:44 +0000

    fix contextinfo

--- src/Utils/messages.ts
+++ src/Utils/messages.ts
@@ -456,6 +456,11 @@ export const generateWAMessageContent = async(
 		m[messageType].contextInfo.mentionedJid = message.mentions
 	}
 
+	if ('contextInfo' in message && !!message.contextInfo) {
+		const [messageType] = Object.keys(m)
+		m[messageType].contextInfo = m[messageType] || { }
+		m[messageType].contextInfo = message.contextInfo
+	}
 	return WAProto.Message.fromObject(m)
 }
 
